c67641d4376a2215c476852bafd305ee2bc3a6a1
Francis Salvador <jaryll_salvador@dlsu.edu.ph && git config --global user.name jarryl>
2024-03-14 20:14:14 +0800

    revised header functionality

--- CCAPDEV MCO/app.js
+++ CCAPDEV MCO/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const server = express();
 
 const bodyParser = require('body-parser');
-server.use(bodyParser.json()); 
+server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
 const handlebars = require('express-handlebars');
+
 server.set('view engine', 'hbs');
 server.engine('hbs', handlebars.engine({
     extname: 'hbs',
@@ -18,28 +19,46 @@ server.use('/login', login);
 
 server.get('/', (req, res) => {
     res.render('main',{
-        layout: 'index'
-      });
-})
 
+        layout: 'index'
+    });
+})
 server.get('/homepage', (req, res) => {
     res.render('main',{
+        user : req.query.user,
         layout: 'homepage'
-      });
+    });
 })
 
-server.get('/profile', (req, res) => {
-    res.render('main',{
-        layout: 'profile'
-      });
-})
+const Profile = require('./profile');
+server.get('/profile', async (req, res) => {
+    try {
+
+        const user = req.query.user
+        console.log(user)
+        const profile = await Profile.findOne({account_name : user}).exec();
+        res.render('main', { layout: 'profile', user: user, display_name: profile.display_name ,account_name:profile.account_name ,profile_email:profile.profile_email, admin_access:profile.admin_access, student_access:profile.student_access,profile_picture:profile.profile_picture });
+
+    } catch (err) {
+        console.error('Error retrieving user profile:', err);
+        res.status(500).send('Error retrieving user profile');
+    }
+});
+
 
 server.get('/reserve_seat', (req, res) => {
     res.render('main',{
+        user : req.query.user,
         layout: 'reserve_seat'
-      });
+    });
 })
 
+server.get('/editprofile', (req, res) => {
+    res.render('main',{
+        user : req.query.user,
+        layout: 'editprofile'
+    });
+})
 const port = process.env.PORT | 9090;
 server.listen(port, function(){
     console.log('Listening at port '+port);
--- CCAPDEV MCO/login.js
+++ CCAPDEV MCO/login.js
@@ -3,8 +3,8 @@ const express = require('express');
 const server = express.Router();
 
 const loginSchema = new mongoose.Schema({
-  user: { type: String },
-  pass: { type: String }
+    user: { type: String },
+    pass: { type: String }
 },{ versionKey: false });
 
 const loginModel = mongoose.model('login', loginSchema);
@@ -13,21 +13,22 @@ const loginModel = mongoose.model('login', loginSchema);
 
 server.post('/read-user', (req, resp) => {
     const searchQuery = { user: req.body.user, pass: req.body.pass };
-  
-    loginModel.findOne(searchQuery).then(function(login){
-      console.log('Finding user');
-  
-      if(login != undefined && login._id != null){
-        // resp.send('username and password match');
-        
-        // - TODO: sa gagawa ng profile, change this nalang to pass the data u need from mongoose
-        resp.redirect('/homepage') 
 
-      }else{
-      // - TODO: error screen
-        resp.send('[ERROR] not match'); 
-      }
+
+    loginModel.findOne(searchQuery).then(function(login){
+        console.log('Finding user');
+
+        if(login != undefined && login._id != null){
+            // resp.send('username and password match');
+
+            // - TODO: sa gagawa ng profile, change this nalang to pass the data u need from mongoose
+            resp.redirect(`/profile?user=${req.body.user}`)
+
+        }else{
+            // - TODO: error screen
+            resp.send('[ERROR] not match');
+        }
     }).catch(err => {throw err});
-  });
-  
-  module.exports = server
+});
+
+module.exports = server
