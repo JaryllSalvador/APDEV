5ac536039e7b00a8e9a0949bbfcde5ce20ca58ad
Jaryll Salvador <125327484+JaryllSalvador@users.noreply.github.com>
2024-03-14 21:07:27 +0800

    Add files via upload

--- CCAPDEV MCO/app.js
+++ CCAPDEV MCO/app.js
@@ -23,10 +23,10 @@ server.get('/', (req, res) => {
         layout: 'index'
     });
 })
-server.get('/homepage', (req, res) => {
+server.get('/reserve_seat', (req, res) => {
     res.render('main',{
         user : req.query.user,
-        layout: 'homepage'
+        layout: 'reserve_seat'
     });
 })
 
@@ -44,20 +44,35 @@ server.get('/profile', async (req, res) => {
         res.status(500).send('Error retrieving user profile');
     }
 });
-
-
-server.get('/reserve_seat', (req, res) => {
+server.get('/homepage', (req, res) => {
     res.render('main',{
         user : req.query.user,
-        layout: 'reserve_seat'
-    });
+        displayname : req.query.display_name,
+            layout: 'homepage'
+});
 })
 
-server.get('/editprofile', (req, res) => {
-    res.render('main',{
-        user : req.query.user,
-        layout: 'editprofile'
-    });
+server.get('/editprofile', async (req, res) => {
+    try {
+
+        const user = req.query.user
+        console.log(user)
+        const profile = await Profile.findOne({account_name: user}).exec();
+        res.render('main', {
+            layout: 'editprofile',
+            user: user,
+            display_name: profile.display_name,
+            account_name: profile.account_name,
+            profile_email: profile.profile_email,
+            admin_access: profile.admin_access,
+            student_access: profile.student_access,
+            profile_picture: profile.profile_picture
+        });
+
+    } catch (err) {
+        console.error('Error retrieving user profile:', err);
+        res.status(500).send('Error retrieving user profile');
+    }
 })
 const port = process.env.PORT | 9090;
 server.listen(port, function(){
